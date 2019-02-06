ff975d123424aed7b707e4260b1245c3918f9f04
Andrea Yang <firecomet185@gmail.com>
2019-02-05 23:43:46 -0700

    Create game screen

---
+++ packages/client/src/GameScreen.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const exampleGameState = {
+    round: 1,
+    totalRounds: 12,
+    gameLeaderPlayerId: '',
+    gameStatus: 'IN_PROGRESS',
+    playerStates: {
+        'andrealized': {
+            id: 'andrealized',
+            cards: [15,27,22]
+        },
+        'jmoneyswagtime': {
+            id: 'jmoneyswagtime',
+            cards: [4,89,24]
+        }
+    },
+    discardedCards: [1, 5, 6],
+    lives: 2
+}
+
+const myUserId = 'andrealized';
+
+class GameScreen extends Component {
+
+  joinGame() {
+
+  }
+
+  render() {
+
+    let myCards = exampleGameState.playerStates[myUserId].cards;
+    let outOfCards = !myCards.length;
+
+    return (
+      <div className="gameBody">
+        <h1>The Mind</h1>
+        <div className="gameSection">
+          <h3 className="roundAndLives">Round: {exampleGameState.round} of 12 &nbsp;&nbsp;&nbsp;Lives: {exampleGameState.lives}</h3>
+
+          {Object.values(exampleGameState.playerStates).map(player =>
+            <div className="playerList">
+             <div className="playerListName">
+              { player.id === myUserId && <span>*</span> }
+              {player.id}
+             </div>
+              <div>{player.cards.length} cards left</div>
+            </div>
+          )}
+        </div>
+
+        <div className="gameSection">
+          <h3>Last Card Played</h3>
+          <div className="cardNumber">
+            {exampleGameState.discardedCards[exampleGameState.discardedCards.length - 1]}
+          </div>
+        </div>
+
+        <div className="gameSection">
+          <h3>Your Cards</h3>
+          <div className="cardNumber">
+            {myCards.join(' ')}
+          </div>
+        </div>
+
+        { !outOfCards &&
+          <div className="gameSection">
+            <p>Push the button if you think you hold the lowest card in your hand.</p>
+            <button onClick={this.joinGame.bind(this)}>Play Lowest Card</button>
+          </div>
+        }
+      </div>
+    );
+  }
+}
+
+export default GameScreen;
