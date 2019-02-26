d943c794888fd41b2a1fbd720f2198fcf889b119
Jeffrey Young <jeffreeyoung@yahoo.com>
2019-02-26 12:59:40 -0700

    meow

--- packages/client/src/WaitingRoomScreen.js
+++ packages/client/src/WaitingRoomScreen.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import { getUserName } from './userNameFunctions';
 
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
 class WaitingRoomScreen extends Component {
 
   shouldShowStartGameButton() {
@@ -17,17 +19,31 @@ class WaitingRoomScreen extends Component {
   }
 
   render() {
-    
+
     return (
       <div className="gameBody">
         <h1>The Mind</h1>
         <p>Waiting for all players to join...</p>
-          {
-            this.getPlayers()
-              .map(name => (<p>{name}</p>))
-          }
+        {
+          this.getPlayers()
+            .map(name => (<p>{name}</p>))
+        }
         <br />
         {this.shouldShowStartGameButton() && <button onClick={this.props.startGame}>Start Game</button>}
+
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <CopyToClipboard
+          text={window.location.href}
+          onCopy={() => {
+            alert('Copied to clipboard!');
+          }}
+        >
+          <button>Copy invite link to clipboard</button>
+        </CopyToClipboard>
       </div>
     );
   }
